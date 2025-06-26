c8cf773f98c9e88011a69fdf2ac2739708827b96
natan-teklie <116352605+natan-teklie@users.noreply.github.com>
2025-06-26 22:31:11 +0300

    firstCommit
    it is the initial stage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-blue-500'>NATAN IS + A REAL SOFTWARE DEVELOPER</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
